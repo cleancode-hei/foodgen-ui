14e44e2bb6b1138dd0278fd69f7a5d322ba75daa
vohizy <hei.vohizy@gmail.com>
2024-02-12 14:10:40 +0300

    feat: create provider user Whoami

--- src/providers/user.ts
+++ src/providers/user.ts
@@ -5,8 +5,14 @@ export const Auth = (token: string): {} => ({
     Authorization: "Bearer " + token,
   },
 });
+
+type Role = "USER" | "ADMIN";
+
 export type Object = User;
 
+export type Objectwhoami = User & {
+  role: Role;
+};
 export type CreatePayload = Omit<User, "id">;
 
 export type UserLogin = {
@@ -24,4 +30,11 @@ export const loginUser = async (userLogin: UserLogin) => {
   return await InstanceAxiosUrl.post<String>("/users/login", userLogin).then(
     (res) => res.data,
   );
+};
+
+export const whoami = async (token: string) => {
+  return await InstanceAxiosUrl.get<Objectwhoami>(
+    "/users/whoami",
+    Auth(token),
+  ).then((res) => res.data);
 };
