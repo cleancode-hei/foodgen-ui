7b099cef18c578abca2283e6541330d0fe3540bc
vohizy <hei.vohizy@gmail.com>
2024-02-12 14:02:27 +0300

    feat: create provider userLogin

--- src/providers/user.ts
+++ src/providers/user.ts
@@ -9,8 +9,19 @@ export type Object = User;
 
 export type CreatePayload = Omit<User, "id">;
 
+export type UserLogin = {
+  email: string;
+  password: string;
+};
+
 export const createUsers = async (userCreate: CreatePayload) => {
   return await InstanceAxiosUrl.put<Object>("/users", userCreate).then(
+    (res) => res.data,
+  );
+};
+
+export const loginUser = async (userLogin: UserLogin) => {
+  return await InstanceAxiosUrl.post<String>("/users/login", userLogin).then(
     (res) => res.data,
   );
 };
