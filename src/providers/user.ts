49e08b592eae886e2292436d2ff4cfdf68d212dd
vohizy <hei.vohizy@gmail.com>
2024-02-12 13:43:00 +0300

    feat: Provider for user create

--- src/providers/user.ts
+++ src/providers/user.ts
@@ -1,5 +1,16 @@
+import { InstanceAxiosUrl } from "./axios";
+import { User } from "@/types/user";
 export const Auth = (token: string): {} => ({
   headers: {
     Authorization: "Bearer " + token,
   },
 });
+export type Object = User;
+
+export type CreatePayload = Omit<User, "id">;
+
+export const createUsers = async (userCreate: CreatePayload) => {
+  return await InstanceAxiosUrl.put<Object>("/users", userCreate).then(
+    (res) => res.data,
+  );
+};
