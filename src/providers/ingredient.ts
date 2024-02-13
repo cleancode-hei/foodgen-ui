30c57d017d5caddd1e3f6567a09ecc3e9acbacac
vohizy <hei.vohizy@gmail.com>
2024-02-13 13:57:32 +0300

    feat: provider get all Ingredient

---
+++ src/providers/ingredient.ts
@@ -0,0 +1,16 @@
+import { InstanceAxiosUrl } from "./axios";
+import { Ingredient } from "@/types/ingredient";
+import { Auth } from "@/types/auth";
+
+export type ObjectReturnIngredient = Omit<Ingredient, "id">;
+
+export const getAllIngredient = async (
+  token: string,
+  page: number,
+  page_size: number,
+) => {
+  return await InstanceAxiosUrl.get<Ingredient[]>(
+    `/ingredients?page=${page}&page_size${page_size}`,
+    Auth(token),
+  ).then((res) => res.data);
+};
