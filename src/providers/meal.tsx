8410ea457a1f46229b02170752489f0349b15894
vohizy <hei.vohizy@gmail.com>
2024-02-13 12:41:53 +0300

    feat: all provider for the meals

---
+++ src/providers/meal.tsx
@@ -0,0 +1,26 @@
+import { InstanceAxiosUrl } from "./axios";
+import { Meal } from "@/types/meal";
+import { Auth } from "@/types/auth";
+
+export const getMealRandom = async (token: string) => {
+  return await InstanceAxiosUrl.get<Meal[]>("/meals", Auth(token)).then(
+    (res) => res.data,
+  );
+};
+
+export const getMealByRating = async (
+  token: string,
+  page: number,
+  page_size: number,
+) => {
+  return await InstanceAxiosUrl.get<Meal[]>(
+    `/meals?page=${page}&page_size${page_size}`,
+    Auth(token),
+  ).then((res) => res.data);
+};
+
+export const getMealByid = async (token: string, id: number) => {
+  return await InstanceAxiosUrl.get<Meal[]>(`/meals/${id}`, Auth(token)).then(
+    (res) => res.data,
+  );
+};
