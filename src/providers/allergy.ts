4bd49c0481d923ff2d4e6144ee467add9733e991
vohizy <hei.vohizy@gmail.com>
2024-02-14 06:05:21 +0300

    fix: use provider for allergie

--- src/providers/allergy.ts
+++ src/providers/allergy.ts
@@ -1,35 +1,43 @@
+import { Provider } from "@/types";
 import { InstanceAxiosUrl } from "./axios";
 import { Allergy } from "@/types/allergy";
-import { Auth } from "@/types/auth";
-
+import { Bearer } from "@/types";
 export type ObjectReturnAllergy = Omit<Allergy, "id">;
 
-export const getAllAllergy = async (
-  token: string,
-  page: number,
-  page_size: number,
-) => {
-  return await InstanceAxiosUrl.get<Allergy[]>(
-    `/allergy?page=${page}&page_size${page_size}`,
-    Auth(token),
-  ).then((res) => res.data);
-};
+export const allergyProvider: Provider = {
+  findMany: async function (params: {
+    token: string;
+    page: number;
+    page_size: number;
+  }): Promise<Allergy[]> {
+    const { token, page, page_size } = params;
+    const response = await InstanceAxiosUrl.get<Allergy[]>(
+      `/allergy?page=${page}&page_size=${page_size}`,
+      Bearer(token),
+    );
+    return response.data;
+  }, // Une fonction pour créer une nouvelle allergie
+  save: async function (resource: {
+    token: string;
+    payload: ObjectReturnAllergy;
+  }): Promise<Allergy> {
+    const { token, payload } = resource;
+    const response = await InstanceAxiosUrl.post<Allergy>(
+      "/allergy",
+      payload,
+      Bearer(token),
+    );
+    return response.data;
+  },
 
-("TODO: request body no defined");
-export const createAllergy = async (
-  token: string,
-  allergyPayload: ObjectReturnAllergy,
-) => {
-  return await InstanceAxiosUrl.put<Allergy[]>(
-    "/allergy",
-    allergyPayload,
-    Auth(token),
-  ).then((res) => res.data);
-};
-
-export const getAllergyByid = async (token: string, id: number) => {
-  return await InstanceAxiosUrl.get<Allergy>(
-    `/allergy/${id}`,
-    Auth(token),
-  ).then((res) => res.data);
+  findOne: async function (params: {
+    token: string;
+    id: number;
+  }): Promise<Allergy> {
+    const { token, id } = params;
+    return await InstanceAxiosUrl.get<Allergy>(
+      `/allergy/${id}`,
+      Bearer(token),
+    ).then((res) => res.data);
+  },
 };
