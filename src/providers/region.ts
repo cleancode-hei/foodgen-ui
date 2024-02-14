e4957907c3438d7cfb956c1b3aa8c52c2e9b3e94
vohizy <hei.vohizy@gmail.com>
2024-02-14 14:10:17 +0300

    refactore: refactore implementation region provider

--- src/providers/region.ts
+++ src/providers/region.ts
@@ -1,31 +1,54 @@
+import { Bearer, Provider, Resource } from "@/types";
 import { InstanceAxiosUrl } from "./axios";
 import { Region } from "@/types/region";
-import { Auth } from "@/types/auth";
 
-export const getAllRegions = async (
-  token: string,
-  page: number,
-  page_size: number,
-) => {
-  return await InstanceAxiosUrl.put<Region[]>(
-    `/regions?page=${page}&page_size${page_size}`,
-    Auth(token),
-  ).then((res) => res.data);
+export type PayloadRegion = {
+  name: string;
 };
-export const createRegion = async (token: string, name: string) => {
-  return await InstanceAxiosUrl.put<Region[]>(
-    "/regions",
-    name,
-    Auth(token),
-  ).then((res) => res.data);
-};
-export const getRegionByid = async (token: string, id: number) => {
-  return await InstanceAxiosUrl.get<Region>(`/regions/${id}`, Auth(token)).then(
-    (res) => res.data,
-  );
-};
-export const deleteRegionById = async (token: string, id: number) => {
-  return await InstanceAxiosUrl.get<string>(`/regions/${id}`, Auth(token)).then(
-    (res) => res.data,
-  );
+export type ListPayloadRegion = PayloadRegion[];
+export const regionProvider: Provider<ListPayloadRegion, Region[], Region[]> = {
+  findMany: async function (params: {
+    token: string;
+    page: number;
+    page_size: number;
+  }): Promise<Region[]> {
+    const { token, page, page_size } = params;
+    const response = await InstanceAxiosUrl.get<Region[]>(
+      `/regions?page=${page}&page_size=${page_size}`,
+      Bearer(token),
+    );
+    return response.data;
+  },
+  save: async function (
+    resource: Resource<ListPayloadRegion>,
+  ): Promise<Region[]> {
+    const { token, payload } = resource;
+    const response = await InstanceAxiosUrl.put<Region[]>(
+      "/regions",
+      payload,
+      Bearer(token),
+    );
+    return response.data;
+  },
+
+  findOne: async function (params: {
+    token: string;
+    id: string;
+  }): Promise<Region[]> {
+    const { token, id } = params;
+    return await InstanceAxiosUrl.get<Region[]>(
+      `/regions/${id}`,
+      Bearer(token),
+    ).then((res) => res.data);
+  },
+  delete: async function (params: {
+    token: string;
+    id: string;
+  }): Promise<Region[]> {
+    const { token, id } = params;
+    return await InstanceAxiosUrl.get<Region[]>(
+      `/regions/${id}`,
+      Bearer(token),
+    ).then((res) => res.data);
+  },
 };
