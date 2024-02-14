86953a1ce1a5e9e0cabf75a8c97bb50231fe6dff
vohizy <hei.vohizy@gmail.com>
2024-02-14 12:48:26 +0300

    refactor: refactor provider of ingredient

--- src/providers/ingredient.ts
+++ src/providers/ingredient.ts
@@ -1,34 +1,48 @@
 import { InstanceAxiosUrl } from "./axios";
 import { Ingredient } from "@/types/ingredient";
-import { Auth } from "@/types/auth";
+import { Bearer, Provider, Resource } from "@/types";
 
-export type ObjectReturnIngredient = Omit<Ingredient, "id">;
+export type payloadIngredient = Ingredient;
+export type Item = { name: string };
+export type ItemList = Item[];
 
-export const getAllIngredient = async (
-  token: string,
-  page: number,
-  page_size: number,
-) => {
-  return await InstanceAxiosUrl.get<Ingredient[]>(
-    `/ingredients?page=${page}&page_size${page_size}`,
-    Auth(token),
-  ).then((res) => res.data);
-};
-
-export const createIngredient = async (
-  token: string,
-  ingredientPayload: ObjectReturnIngredient,
-) => {
-  return await InstanceAxiosUrl.put<Ingredient[]>(
-    "/ingredients",
-    ingredientPayload,
-    Auth(token),
-  ).then((res) => res.data);
-};
-
-export const getIngredientByid = async (token: string, id: number) => {
-  return await InstanceAxiosUrl.get<Ingredient>(
-    `/ingredients/${id}`,
-    Auth(token),
-  ).then((res) => res.data);
+export const ingredientProvider: Provider<
+  ItemList,
+  payloadIngredient[],
+  Ingredient[]
+> = {
+  findMany: async function (params: {
+    token: string;
+    page: number;
+    page_size: number;
+  }): Promise<Ingredient[]> {
+    const { token, page, page_size } = params;
+    const response = await InstanceAxiosUrl.get<Ingredient[]>(
+      `/ingredients?page=${page}&page_size=${page_size}`,
+      Bearer(token),
+    );
+    return response.data;
+  },
+  findOne: async function (params: {
+    token: string;
+    id: string;
+  }): Promise<Ingredient[]> {
+    const { token, id } = params;
+    const response = await InstanceAxiosUrl.get<Ingredient[]>(
+      `/ingredients/${id}`,
+      Bearer(token),
+    );
+    return response.data;
+  },
+  save: async function (
+    resource: Resource<ItemList>,
+  ): Promise<payloadIngredient[]> {
+    const { token, payload } = resource;
+    const response = await InstanceAxiosUrl.put<payloadIngredient[]>(
+      "/allergy",
+      payload,
+      Bearer(token),
+    );
+    return response.data;
+  },
 };
