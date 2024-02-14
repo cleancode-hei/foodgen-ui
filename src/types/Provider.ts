0d3fe29d6a608754f44e6455803664b07de23ff0
vohizy <hei.vohizy@gmail.com>
2024-02-14 10:03:02 +0300

    chore: change type of retour Promise

--- src/types/Provider.ts
+++ src/types/Provider.ts
@@ -13,14 +13,14 @@ export interface Resource<T> {
 }
 
 export type Provider<T, R> = {
-  findMany?: (params: Params) => Promise<void | unknown>;
-  findOne?: (params: Params) => Promise<void | unknown>;
+  findMany?: (params: Params) => Promise<R>;
+  findOne?: (params: Params) => Promise<R>;
   save?: (resource: Resource<T>) => Promise<R>;
   saveMany?: (resources: Resource<T>) => Promise<R>;
   update?: (resource: Resource<T>) => Promise<R>;
   updateMany?: (resources: Resource<T>) => Promise<R>;
   delete?: (resource: Resource<T>) => Promise<R>;
   deleteMany?: (resources: Resource<T>) => Promise<R>;
-  login?: (auth: Auth) => Promise<void | unknown>;
-  logout?: () => Promise<void | unknown>;
+  login?: (auth: Auth) => Promise<R>;
+  logout?: () => Promise<R>;
 };
