63826361368f9221c07ecf4c2dc244591629f57b
vohizy <hei.vohizy@gmail.com>
2024-02-15 07:48:33 +0300

    chore: implement the methode for handleAxiosError

--- src/providers/user.ts
+++ src/providers/user.ts
@@ -1,6 +1,8 @@
 import { ListCreateUserPayload, User } from "@/types/user";
 import { InstanceAxiosUrl } from "./axios";
 import { Bearer, Provider, Resource } from "@/types";
+import { handleAxiosError } from "@/lib/handleAxiosError";
+import { AxiosError } from "axios";
 
 export const userProvider: Provider<ListCreateUserPayload, User, User> = {
   findMany: async function (params: {
@@ -8,25 +10,38 @@ export const userProvider: Provider<ListCreateUserPayload, User, User> = {
     username: string;
   }): Promise<User> {
     const { token, username } = params;
-    const response = await InstanceAxiosUrl.get<User>(
-      `/users/${username}`,
-      Bearer(token),
-    );
-    return response.data;
+    try {
+      const response = await InstanceAxiosUrl.get<User>(
+        `/users/${username}`,
+        Bearer(token),
+      );
+      return response.data;
+    } catch (error) {
+      throw handleAxiosError(error as AxiosError);
+    }
   },
   save: async function (
     resource: Resource<ListCreateUserPayload>,
   ): Promise<User> {
     const { payload } = resource;
-    const response = await InstanceAxiosUrl.put<User>("/users", payload);
-    return response.data;
+    try {
+      const response = await InstanceAxiosUrl.put<User>("/users", payload);
+      return response.data;
+    } catch (error) {
+      handleAxiosError(error as AxiosError);
+    }
   },
 
   findOne: async function (params: { token: string }): Promise<User> {
     const { token } = params;
-    return await InstanceAxiosUrl.get<User>(
-      `/users/Whoami`,
-      Bearer(token),
-    ).then((res) => res.data);
+    try {
+      const response = await InstanceAxiosUrl.get<User>(
+        `/users/Whoami`,
+        Bearer(token),
+      );
+      return response.data;
+    } catch (error) {
+      handleAxiosError(error as AxiosError);
+    }
   },
 };
