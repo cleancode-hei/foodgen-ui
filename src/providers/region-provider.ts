c60ef4834738a1a2750d3ca92d995ebc599e75f4
Sanda Mandresy <98638893+Sanmandresy@users.noreply.github.com>
2024-02-18 16:03:34 +0300

    chore: refactor providers

--- src/providers/region-provider.ts
+++ src/providers/region-provider.ts
@@ -1,15 +1,14 @@
-import { Bearer, Provider, Resource } from "@/types";
-import { ListPayloadRegion, Region } from "@/types/region";
+import { Bearer, Pagination } from "@/types";
+import { Region } from "@/types";
 import { handleAxiosError } from "@/lib/handleAxiosError";
 import { AxiosError } from "axios";
-import { api } from "@/lib/Api";
-export const regionProvider: Provider<ListPayloadRegion, Region, Region[]> = {
-  findMany: async (params: {
-    token: string;
-    page: number;
-    page_size: number;
-  }): Promise<Region[]> => {
-    const { token, page, page_size } = params;
+import { api } from "@/lib";
+export const regionProvider = {
+  findMany: async (
+    pagination: Pagination,
+    token: string,
+  ): Promise<Region[]> => {
+    const { page, page_size } = pagination;
     try {
       const response = await api.get<Region[]>(
         `/regions?page=${page}&page_size=${page_size}`,
@@ -20,12 +19,11 @@ export const regionProvider: Provider<ListPayloadRegion, Region, Region[]> = {
       handleAxiosError(error as AxiosError);
     }
   },
-  save: async (resource: Resource<ListPayloadRegion>): Promise<Region[]> => {
-    const { token, payload } = resource;
+  saveOrUpdate: async (regions: Region[], token: string): Promise<Region[]> => {
     try {
       const response = await api.put<Region[]>(
         "/regions",
-        payload,
+        regions,
         Bearer(token),
       );
       return response.data;
@@ -34,18 +32,15 @@ export const regionProvider: Provider<ListPayloadRegion, Region, Region[]> = {
     }
   },
 
-  findOne: async (params: { token: string; id: string }): Promise<Region> => {
-    const { token, id } = params;
+  findOne: async (id: string, token: string): Promise<Region> => {
     try {
       const response = await api.get<Region>(`/regions/${id}`, Bearer(token));
-
       return response.data;
     } catch (error) {
       handleAxiosError(error as AxiosError);
     }
   },
-  delete: async (params: { token: string; id: string }): Promise<string> => {
-    const { token, id } = params;
+  delete: async (id: string, token: string): Promise<string> => {
     try {
       const response = await api.get<string>(`/regions/${id}`, Bearer(token));
       return response.data;
